10542f637712dcfa4edfeeb2a237637348ef3f91
id2k1149 <id2k1149@gmail.com>
2023-04-03 13:08:23 -0400

    add black square view

--- Hourglass_v01/ViewController.swift
+++ Hourglass_v01/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let squareView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .cyan
+        addBlackSquareView(using: squareView)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+       
+        squareView.layer.cornerRadius = squareView.frame.width / 4
     }
 
-
+    
 }
 
+extension UIViewController {
+    func addBlackSquareView(using subview: UIView) {
+        
+        let minSize = min(view.frame.height, view.frame.width) * 0.95
+        
+        view.addSubview(subview)
+        subview.backgroundColor = .black
+        subview.translatesAutoresizingMaskIntoConstraints = false
+
+        subview.widthAnchor.constraint(equalToConstant: minSize).isActive = true
+        subview.heightAnchor.constraint(equalTo: subview.widthAnchor).isActive = true
+
+        subview.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        subview.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
+ 
